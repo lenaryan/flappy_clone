b56d657fe414ec59d419afc6f1c8762db734cd95
Lena Ryan <alissadyiphil@gmail.com>
2023-08-15 18:02:37 +0600

    added sky and bird position

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import Phaser from "phaser";
 
 const config = {
@@ -6,23 +5,22 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    default: 'arcade'
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create,
   }
-};
+}
+
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+}
+
+function create() {
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  this.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0.5);
+}
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
