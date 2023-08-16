e9d5eee88c45fff45ad5593079bf211a6f60b1ed
Lena Ryan <alissadyiphil@gmail.com>
2023-08-16 18:57:59 +0600

    added flapping feature

--- src/index.js
+++ src/index.js
@@ -5,22 +5,56 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade'
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
   },
   scene: {
     preload,
     create,
+    update,
   }
 }
+
+const VELOCITY = 200;
+let bird = null;
+let upperPipe = null;
+let lowerPipe = null;
+const flapVelocity = 250;
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2};
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  this.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0.5);
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0.5);
+  bird.body.gravity.y = 400;
+  upperPipe = this.physics.add.sprite(400, 100, 'pipe').setOrigin(0, 1);
+  lowerPipe = this.physics.add.sprite(400, upperPipe.y + 100, 'pipe').setOrigin(0);
+
+  this.input.on('pointerdown', flap)
+
+  const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  spaceBar.on('down', flap)
+}
+
+function update(time, delta) {
+  if (bird.y > config.height || bird.y < -bird.height) restartBirdPosition();
+}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity;
+}
+
+function restartBirdPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
 }
 
 new Phaser.Game(config);
