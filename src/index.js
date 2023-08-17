1bf952b53e6c419104ec037e8c37047381182e76
Lena Ryan <alissadyiphil@gmail.com>
2023-08-17 11:30:30 +0600

    refactoring pipes

--- src/index.js
+++ src/index.js
@@ -19,10 +19,9 @@ const config = {
 
 const PIPES_TO_RENDER = 4;
 let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
-let pipeHorizontalDistance = 0;
+let pipes = null;
 const pipeVerticalDistanceRange = [150, 250];
+const pipeHorizontalDistanceRange = [350, 400];
 const flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2};
 
@@ -37,16 +36,16 @@ function create() {
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0.5);
   bird.body.gravity.y = 400;
 
+  pipes = this.physics.add.group();
+
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    pipeHorizontalDistance += 300;
-    const pipeVerticalDistance = Phaser.Math.Between(pipeVerticalDistanceRange[0], pipeVerticalDistanceRange[1]);
-    const pipeVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
-    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-    lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0);
-    
-    upperPipe.body.velocity.x = -200;
-    lowerPipe.body.velocity.x = -200;   
+    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0);
+
+    placePipe(upperPipe, lowerPipe);
   }
+
+  pipes.setVelocityX(-200);
 
   const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
   this.input.on('pointerdown', flap);
@@ -65,6 +64,28 @@ function restartBirdPosition() {
   bird.x = initialBirdPosition.x;
   bird.y = initialBirdPosition.y;
   bird.body.velocity.y = 0;
+}
+
+function placePipe(upper, lower) {
+  const rightMostX = getRightMostPipe();
+  const pipeHorizontalDistance = Phaser.Math.Between(pipeHorizontalDistanceRange[0], pipeHorizontalDistanceRange[1])
+  const pipeVerticalDistance = Phaser.Math.Between(pipeVerticalDistanceRange[0], pipeVerticalDistanceRange[1]);
+  const pipeVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
+
+  upper.x = rightMostX + pipeHorizontalDistance;
+  lower.x = upper.x;
+  upper.y = pipeVerticalPosition;
+  lower.y = upper.y + pipeVerticalDistance;  
+}
+
+function getRightMostPipe() {
+  let rightX = 0;
+
+  pipes.getChildren().forEach(function(pipe) {
+    rightX = Math.max(pipe.x, rightX);
+  })
+
+  return rightX;
 }
 
 new Phaser.Game(config);
