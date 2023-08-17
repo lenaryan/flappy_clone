ad65b8bafc5ca1dd53a767b801250019bb683e64
Lena Ryan <alissadyiphil@gmail.com>
2023-08-17 13:14:19 +0600

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,103 +1,26 @@
 import Phaser from "phaser";
 import PlayScene from "./scenes/PlayScene";
 
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = {x: WIDTH * 0.1, y: HEIGHT / 2}
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION
+}
+
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
   physics: {
     default: 'arcade',
     arcade: {
       debug: true
     }
   },
-  scene: [PlayScene]
-}
-
-const PIPES_TO_RENDER = 4;
-let bird = null;
-let pipes = null;
-const pipeVerticalDistanceRange = [150, 250];
-const pipeHorizontalDistanceRange = [350, 400];
-const flapVelocity = 250;
-const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2};
-
-function preload() {
-  this.load.image('pipe', 'assets/pipe.png');
-}
-
-function create() {
-  pipes = this.physics.add.group();
-
-  for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
-    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0);
-
-    placePipe(upperPipe, lowerPipe);
-  }
-
-  pipes.setVelocityX(-200);
-
-  const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-  this.input.on('pointerdown', flap);
-  spaceBar.on('down', flap);
-}
-
-function update(time, delta) {
-  if (bird.y > config.height || bird.y < -bird.height) restartBirdPosition();
-  recyclePipes();
-}
-
-function flap() {
-  bird.body.velocity.y = -flapVelocity;
-}
-
-function restartBirdPosition() {
-  bird.x = initialBirdPosition.x;
-  bird.y = initialBirdPosition.y;
-  bird.body.velocity.y = 0;
-}
-
-function placePipe(upper, lower) {
-  // TODO: wtf, it's looping through the whole pipe array every time to get X
-  // probably it's easier to get position of the last pipe only
-  const rightMostX = getRightMostPipe();
-  const pipeHorizontalDistance = Phaser.Math.Between(pipeHorizontalDistanceRange[0], pipeHorizontalDistanceRange[1])
-  const pipeVerticalDistance = Phaser.Math.Between(pipeVerticalDistanceRange[0], pipeVerticalDistanceRange[1]);
-  const pipeVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
-
-  upper.x = rightMostX + pipeHorizontalDistance;
-  lower.x = upper.x;
-  upper.y = pipeVerticalPosition;
-  lower.y = upper.y + pipeVerticalDistance;  
-}
-
-function recyclePipes() {
-  // TODO: maybe look at the first one only
-  // for not to loop through the whole array every fucking second
-  // remove the first one after placing pipe
-  const tempPipes = [];
-  pipes.getChildren().forEach(pipe => {
-    if (pipe.getBounds().right <= 0) {
-      // recycle
-      tempPipes.push(pipe);
-      if (tempPipes.length === 2) {
-        // why is it working with no cleaning of the temp array?
-        placePipe(...tempPipes);
-      }
-      debugger
-    }
-  })
-}
-
-function getRightMostPipe() {
-  let rightX = 0;
-
-  pipes.getChildren().forEach(function(pipe) {
-    rightX = Math.max(pipe.x, rightX);
-  })
-
-  return rightX;
+  scene: [new PlayScene(SHARED_CONFIG)]
 }
 
 new Phaser.Game(config);
--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -1,29 +1,120 @@
 import Phaser from "phaser";
 
-class PlayScene extends Phaser.Scene {
-    constructor() {
-        super('PlayScene');
-        this.initialBirdPosition = {
-            x: 80,
-            y: 300,
-        }
+const PIPES_TO_RENDER = 4;
+const FLAP_VELOCITY = 250;
 
+class PlayScene extends Phaser.Scene {
+    constructor(config) {
+        super('PlayScene');
+        this.config = config;
         this.bird = null;
+        this.pipes = null;
+
+        this.pipeVerticalDistanceRange = [150, 250];
+        this.pipeHorizontalDistanceRange = [350, 400];
     }
 
     preload() {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('bird', 'assets/bird.png');
+        this.load.image('pipe', 'assets/pipe.png');
     }
 
     create() {
+        this.createBG();
+        this.createBird();
+        this.createPipes();
+        this.handleInputs();
+    }
+
+    update(time, delta) {
+        this.checkGameStatus();
+        this.recyclePipes();
+    }
+
+    createBG() {
         this.add.image(0, 0, 'sky').setOrigin(0);
-        this.bird = this.physics.add.sprite(this.initialBirdPosition.x, this.initialBirdPosition.y, 'bird').setOrigin(0.5);
+    }
+
+    createBird() {
+        this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0.5);
         this.bird.body.gravity.y = 400;
     }
 
-    update() {
+    createPipes() {
+        this.pipes = this.physics.add.group();
+
+        for (let i = 0; i < PIPES_TO_RENDER; i++) {
+          const upperPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+          const lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0);
+      
+          this.placePipe(upperPipe, lowerPipe);
+        }
+      
+        this.pipes.setVelocityX(-200);
+    }
+
+    handleInputs() {
+        const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.input.on('pointerdown', this.flap, this);
+        spaceBar.on('down', this.flap, this);
+    }
+
+    placePipe(upper, lower) {
+        // TODO: wtf, it's looping through the whole pipe array every time to get X
+        // probably it's easier to get position of the last pipe only
+        const rightMostX = this.getRightMostPipe();
+        const pipeHorizontalDistance = Phaser.Math.Between(this.pipeHorizontalDistanceRange[0], this.pipeHorizontalDistanceRange[1])
+        const pipeVerticalDistance = Phaser.Math.Between(this.pipeVerticalDistanceRange[0], this.pipeVerticalDistanceRange[1]);
+        const pipeVerticalPosition = Phaser.Math.Between(20, this.config.height - 20 - pipeVerticalDistance);
+      
+        upper.x = rightMostX + pipeHorizontalDistance;
+        lower.x = upper.x;
+        upper.y = pipeVerticalPosition;
+        lower.y = upper.y + pipeVerticalDistance;  
+    }
+
+    recyclePipes() {
+        // TODO: maybe look at the first one only
+        // for not to loop through the whole array every fucking second
+        // remove the first one after placing pipe
+        const tempPipes = [];
+        this.pipes.getChildren().forEach(pipe => {
+          if (pipe.getBounds().right <= 0) {
+            // recycle
+            tempPipes.push(pipe);
+            if (tempPipes.length === 2) {
+              // why is it working with no cleaning of the temp array?
+              this.placePipe(...tempPipes);
+            }
+          }
+        })
+    }
+
+    flap() {
+        this.bird.body.velocity.y = -FLAP_VELOCITY;
+    }
+    
+    restartBirdPosition() {
+        this.bird.x = this.config.startPosition.x;
+        this.bird.y = this.config.startPosition.y;
+        this.bird.body.velocity.y = 0;
+    }
+    
+    getRightMostPipe() {
+        let rightX = 0;
         
+        this.pipes.getChildren().forEach(function(pipe) {
+            rightX = Math.max(pipe.x, rightX);
+        })
+        
+        return rightX;
+    }
+
+    checkGameStatus() {
+        if (this.bird.y > this.config.height || this.bird.y < -this.bird.height) {
+            this.restartBirdPosition();
+        }
     }
 }
 
