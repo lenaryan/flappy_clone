83fcb911df43eaa958fb9c4cbd17dbbb495e7462
Lena Ryan <alissadyiphil@gmail.com>
2023-08-17 17:46:40 +0600

    added menu

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -4,10 +4,27 @@ class BaseScene extends Phaser.Scene {
     constructor(key, config) {
         super(key);
         this.config = config;
+        this.fontSize = '32px';
+        this.lineHeight = 42;
+        this.screenCenter = [config.width / 2, config.height / 2]
     }
 
     create() {
         this.add.image(0, 0, 'sky').setOrigin(0);
+    }
+
+    createMenu(menu, setupMenuEvents) {
+        let lastMenuPositionY = 0;
+
+        menu.forEach(menuItem => {
+            const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY]
+            menuItem.textGO = this.add.text(...menuPosition, menuItem.text, {
+                fontSize: this.fontSize,
+                fill: '#fff',
+            }).setOrigin(0.5, 1);
+            lastMenuPositionY += this.lineHeight;
+            setupMenuEvents(menuItem)
+        })
     }
 }
 
